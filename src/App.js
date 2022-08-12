60035f063ce449863cf1a1c5f906af337909be90
Nikolas <halonikolas@gmail.com>
2022-08-13 00:13:06 +0200

    particles background, tilt logo.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navigation from "./components/navigation/navigation";
-
 import './App.css';
 import Logo from "./components/logo/logo";
 import ImageLinkForm from "./components/imageLinkForm/imageLinkForm";
+import Background from "./components/background/background";
 
 function App() {
+
   return (
     <div className="App">
+        <Background />
         <Navigation />
         <Logo />
         <ImageLinkForm />
--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,16 +1,18 @@
 import React from "react";
-import Tilt from 'react-tilt';
 import './logo.css';
 import brain from './brain.png';
+import Tilt from "react-tilt/dist/tilt";
 
 const Logo = () => {
     return (
-      <div className={'ma4 mt0'}>
-          <Tilt className="Tilt br2 shadow-2 " options={{ max : 55 }} style={{ height: 170, width: 170 }} >
-              <div className="Tilt-inner pa4"><img alt='logo' src={brain}/></div>
-          </Tilt>
-      </div>
-    );
+        <div className='ma4 mt0'>
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-inner pa3">
+                    <img style={{paddingTop: '5px'}} alt='logo' src={brain}/>
+                </div>
+            </Tilt>
+        </div>
+    )
 }
 
 export default Logo;
