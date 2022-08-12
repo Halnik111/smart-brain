e327740a41d6fe94e919429ac347443844d22977
Nikolas <halonikolas@gmail.com>
2022-08-13 00:31:08 +0200

    LinkForm input state.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,38 @@ import './App.css';
 import Logo from "./components/logo/logo";
 import ImageLinkForm from "./components/imageLinkForm/imageLinkForm";
 import Background from "./components/background/background";
+import Rank from "./components/rank/rank";
+import {Component} from "react";
 
-function App() {
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: ''
+        }
+    }
 
-  return (
-    <div className="App">
-        <Background />
-        <Navigation />
-        <Logo />
-        <ImageLinkForm />
-    </div>
-  );
+
+    onInputChange = (event) => {
+        this.setState({input: event.target.value})
+    }
+
+    onSubmit = () => {
+        console.log('click');
+        console.log(this.state.input);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Background/>
+                <Navigation/>
+                <Logo/>
+                <Rank/>
+                <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit}/>
+            </div>
+        );
+    }
 }
 
 export default App;
